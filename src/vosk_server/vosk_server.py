431a4b11e9a18917bb1a69e3cf143ea4bbc07c32
jpsanchezl10 <72088484+jpsanchezl10@users.noreply.github.com>
2024-09-25 12:13:32 -0500

    update

--- src/vosk_server/vosk_server.py
+++ src/vosk_server/vosk_server.py
@@ -1,40 +1,52 @@
-import vosk
 import json
 from fastapi import WebSocket, WebSocketDisconnect
 import logging
 from vosk import Model, KaldiRecognizer, SpkModel
 import numpy as np
+import logging
+import time
 
 # Load models
-VOSK_MODEL_PATH_EN = './src/models/small/vosk-model-small-en-us-0.15'
-VOSK_MODEL_PATH_ES = './src/models/small/vosk-model-small-es-0.42'
+VOSK_MODEL_PATH_EN_MED = './src/models/med/vosk-model-en-us-daanzu-20200905'
+VOSK_MODEL_PATH_EN_SMALL = './src/models/small/vosk-model-small-en-us-0.15'
+VOSK_MODEL_PATH_ES_SMALL = './src/models/small/vosk-model-small-es-0.42'
 
-model_en = vosk.Model(VOSK_MODEL_PATH_EN)
-model_es = vosk.Model(VOSK_MODEL_PATH_ES)
+try:
+    model_en = Model(VOSK_MODEL_PATH_EN_MED)
+except:
+    logging.error("Failed to Import MED VOSK model , importing small instead")
+    model_en = Model(VOSK_MODEL_PATH_EN_SMALL)
+
+model_es = Model(VOSK_MODEL_PATH_ES_SMALL)
 
 models = {
     'en': model_en,
     'es': model_es
 }
 
+
 class VoskStreamingTranscription:
     def __init__(self, websocket: WebSocket, language: str):
         self.websocket = websocket
-        self.rec = vosk.KaldiRecognizer(models[language], 16000)
+        self.rec = KaldiRecognizer(models[language], 16000)
         self.rec.SetMaxAlternatives(1)
+        self.start_time = None
 
     async def start(self):
         try:
             while True:
                 audio_data = await self.websocket.receive_bytes()
+                self.start_time = time.time()
+                
                 if self.rec.AcceptWaveform(audio_data):
                     result = json.loads(self.rec.Result())
                     alternatives = result.get("alternatives", [])
                     transcript = alternatives[0].get("text", "") if alternatives else ""
                     confidence = alternatives[0].get("confidence", 0) if alternatives else 0
                     if transcript:
+                        elapsed_time = time.time() - self.start_time
                         response = {
-                            "duration": result.get("duration", 0.0),
+                            "duration": elapsed_time,
                             "start": result.get("start", 0.0),
                             "is_final": True,
                             "speech_final": True,
@@ -46,8 +58,9 @@ class VoskStreamingTranscription:
                         }
                 else:
                     partial = json.loads(self.rec.PartialResult())
+                    elapsed_time = time.time() - self.start_time
                     response = {
-                        "duration": 0.0,
+                        "duration": elapsed_time,
                         "start": 0.0,
                         "is_final": False,
                         "speech_final": False,
@@ -66,38 +79,34 @@ class VoskStreamingTranscription:
 
 
 
-
 class VoskBatchTranscription:
     def __init__(self, language: str, diarize: bool = False):
-   
         # Initialize the recognizer
-        self.rec = vosk.KaldiRecognizer(models[language], 16000)
+        self.rec = KaldiRecognizer(models[language], 16000)
         self.rec.SetMaxAlternatives(1)
-
         # Load and set the speaker model if diarization is requested
         if diarize:
             try:
                 # Paths to models
                 SPK_MODEL_PATH = "./src/models/speaker_identification/vosk-model-spk-0.4"
-
-                #init the model
+                # init the model
                 self.spk_model = SpkModel(SPK_MODEL_PATH)
                 self.rec.SetSpkModel(self.spk_model)
             except Exception as e:
                 print(f"Error loading speaker model: {str(e)}")
                 raise
-
         self.diarize = diarize
 
     def transcribe(self, audio_data: bytes) -> dict:
+        start_time = time.time()
         if self.rec.AcceptWaveform(audio_data):
             result = json.loads(self.rec.FinalResult())
             alternatives = result.get("alternatives", [])
             transcript = alternatives[0].get("text", "") if alternatives else ""
             confidence = alternatives[0].get("confidence", 0) if alternatives else 0
-            
+            elapsed_time = time.time() - start_time
             response = {
-                "duration": result.get("duration", 0.0),
+                "duration": elapsed_time,
                 "is_final": True,
                 "speech_final": True,
                 "channel": {
@@ -106,13 +115,11 @@ class VoskBatchTranscription:
                     ]
                 }
             }
-            
             if self.diarize and "spk" in result:
                 response["speaker"] = {
                     "x_vector": result["spk"],
                     "frames": result.get("spk_frames", 0)
                 }
-            
             return response
         return {"error": "Failed to transcribe audio"}
 
